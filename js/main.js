6ad7d5e120b5873353f3959809292a5160661a08
Therakar <matteo.pau200@gmail.com>
2022-11-17 15:58:18 +0100

    inserimento link nell'html, scaffolding css, struttura base vue.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 "use strict"
+
+// struttura vue.js
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'//DEBUG
+      }
+    }
+  }).mount('#app')
